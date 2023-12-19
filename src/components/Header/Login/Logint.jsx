37a310c96342a276ef3dca92d0bae2fa0597c2bb
hyeonbin jeong <duafuuu@gmail.com>
2023-12-19 17:26:15 +0900

    add: Home 관련 컴포넌트 나누기

---
+++ src/components/Header/Login/Logint.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Logint = () => {
+  return <div>Logint</div>;
+};
+
+export default Logint;
--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Home } from './../pages/HomePage/Home';
-import { Post } from './../pages/PostPage/Post';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />} />
-        <Route path="/post" element={<Post />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
