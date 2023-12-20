504f75a3fb85af25955a53c29702abef9ffab018
hyeonbin jeong <duafuuu@gmail.com>
2023-12-21 00:32:18 +0900

    style: 헤더 상단 버튼 스타일 구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import UserContext from './../../context/UserContext';
+import imgLogo from '../../assets/Logo.svg';
+import Login from '../Header/Login/Login';
+import Logout from '../Header/Logout/Logout';
+import './Header.css';
+import './Button.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  const { isLogin } = useContext(UserContext);
+
+  return (
+    <header>
+      <div className="max-width">
+        <h1>
+          <Link to="/">
+            <img src={imgLogo} alt="My Blog" />
+          </Link>
+        </h1>
+        <ul>{isLogin ? <Login /> : <Logout />}</ul>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
