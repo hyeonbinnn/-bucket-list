02a9a89a9f10013e359f857ff54ac01fc9bae97c
hyeonbin jeong <duafuuu@gmail.com>
2023-12-21 00:31:16 +0900

    add: 블로그 배너 데이터 불러오기 및 구현

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Banner.css';
 
 const Banner = () => {
-  return <div>Banner</div>;
+  const [blogInfo, setBlogInfo] = useState(undefined);
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/blog')
+      .then((json) => {
+        setBlogInfo(json.data);
+      })
+      .catch((error) => {
+        console.error('블로그 정보를 가져오는 중 에러 발생', error);
+        setBlogInfo(null);
+      });
+  }, []);
+  // console.log(blogInfo);
+
+  return (
+    <div>
+      {blogInfo === undefined ? (
+        <div>로딩중...</div>
+      ) : (
+        <div className="banner">
+          <div className="max-width">
+            <div className="banner-contents">
+              <p className="sub-text">{blogInfo.subTitle}</p>
+              <p className="main-text">{blogInfo.mainTitle}</p>
+              <p className="description">{blogInfo.description}</p>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Banner;
