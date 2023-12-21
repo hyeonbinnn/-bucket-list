c5d3f151ee15f12dc029771c637cc658f1b9bcd8
hyeonbin jeong <duafuuu@gmail.com>
2023-12-21 20:57:31 +0900

    add: header 스타일드 컴포넌트 추가

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { useContext } from 'react';
 import UserContext from './../../context/UserContext';
 import imgLogo from '../../assets/Logo.svg';
 import Login from '../Header/Login/Login';
 import Logout from '../Header/Logout/Logout';
-import './Header.css';
-import './Button.css';
+import * as S from './Header.style';
 
 const Header = () => {
   const { isLogin } = useContext(UserContext);
 
   return (
-    <header>
-      <div className="max-width">
-        <h1>
-          <Link to="/">
-            <img src={imgLogo} alt="My Blog" />
-          </Link>
-        </h1>
-        <ul>{isLogin ? <Login /> : <Logout />}</ul>
-      </div>
-    </header>
+    <S.Header>
+      <S.Container className="max-width">
+        <S.H1>
+          <S.HeaderLink to="/">
+            <S.HeaderImg src={imgLogo} alt="My Blog" />
+          </S.HeaderLink>
+        </S.H1>
+        <S.HeaderUl>{isLogin ? <Login /> : <Logout />}</S.HeaderUl>
+      </S.Container>
+    </S.Header>
   );
 };
 
