3f614a87cbee91d7898b20c00888482f55be4adf
hyeonbin jeong <duafuuu@gmail.com>
2023-12-25 23:53:56 +0900

    add: 스크롤 탑 추가

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,11 +2,20 @@ import React from 'react';
 import * as S from './Footer.style';
 
 const Footer = () => {
+  const moveToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <S.Footer>
       <S.Container className="max-width">
         <S.H2>© Weniv Corp.</S.H2>
-        <S.TopButton className="top-button">TOP</S.TopButton>
+        <S.TopButton onClick={moveToTop} className="top-button">
+          TOP
+        </S.TopButton>
       </S.Container>
     </S.Footer>
   );
