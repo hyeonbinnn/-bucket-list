9939c9d37768431b3ed5318b5a4e65ac429fc060
hyeonbin jeong <duafuuu@gmail.com>
2023-12-25 23:26:58 +0900

    style: View 컴포넌트에서 style 관련 prop 받기

--- src/components/Main/Category/Category.jsx
+++ src/components/Main/Category/Category.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import * as S from './Category.style';
 
-const Category = ({ category }) => {
+const Category = ({ category, style }) => {
   return (
     <>
-      <S.Category className="category">
+      <S.Category className="category" style={style}>
         <dt className="a11y-hidden">Category</dt>
         {category.map((item, index) => (
           <S.Item key={index}>{item}</S.Item>
